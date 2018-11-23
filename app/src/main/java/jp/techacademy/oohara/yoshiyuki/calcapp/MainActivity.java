543984ff84273ecf48accf1d4d9c1c221804b834
YoshiyukiOhara <parallel.0917@gmail.com>
2018-11-23 23:45:11 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/oohara/yoshiyuki/calcapp/MainActivity.java
@@ -0,0 +1,92 @@
+package jp.techacademy.oohara.yoshiyuki.calcapp;
+
+import android.util.Log;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText editText1;
+    EditText editText2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+
+        Button button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(this);
+
+        editText1 = (EditText) findViewById(R.id.editText1);
+
+        editText2 = (EditText) findViewById(R.id.editText2);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // editTextに入力された値をString型に変換
+        String str1 = editText1.getText().toString();
+        String str2 = editText2.getText().toString();
+
+        // editTextが未入力ならアラートを表示
+        if (str1.equals("") || str2.equals("")) {
+            showAlertDialog();
+        } else {
+            Intent intent = new Intent(this, SecondActivity.class);
+
+            // strをdouble型に変換
+            double value1 = Double.parseDouble(str1);
+            double value2 = Double.parseDouble(str2);
+
+            // 各ボタンをタップした時の処理
+            double result = 0;
+            if (v.getId() == R.id.button1) {
+                result = value1 + value2;
+            } else if (v.getId() == R.id.button2) {
+                result = value1 - value2;
+            } else if (v.getId() == R.id.button3) {
+                result = value1 * value2;
+            } else if (v.getId() == R.id.button4) {
+                result = value1 / value2;
+            }
+
+            intent.putExtra("VALUE", result);
+            startActivity(intent);
+        }
+    }
+
+    private void showAlertDialog() {
+        // AlertDialog.Builderクラスを使ってAlertDialogの準備をする
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setTitle("未入力");
+        alertDialogBuilder.setMessage("何か数値を入力してください");
+
+        // OKボタンに表示される文字列、押したときのリスナーを設定する
+        alertDialogBuilder.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.d("UI_PARTS", "OKボタン");
+                    }
+                });
+
+        // AlertDialogを作成して表示する
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+}
